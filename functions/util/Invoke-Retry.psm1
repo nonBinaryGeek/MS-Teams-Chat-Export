17a2c698d44ce7b7d77e73d6e8145bf363e37f50
nonBinaryGeek <65196866+nonBinaryGeek@users.noreply.github.com>
2024-06-26 10:24:18 -0400

    Create Invoke-Retry.psm1

---
+++ functions/util/Invoke-Retry.psm1
@@ -0,0 +1,32 @@
+[cmdletbinding()]
+Param([bool]$verbose)
+$VerbosePreference = if ($verbose) { 'Continue' } else { 'SilentlyContinue' }
+$ProgressPreference = "SilentlyContinue"
+
+function Invoke-Retry {
+    param(
+        [ScriptBlock]$code,
+        [int]$maxRetries = 2,
+        [int]$delaySeconds = 1
+    )
+
+    $retryCount = 0
+
+    while ($retryCount -lt $maxRetries) {
+        try {
+            & $code
+            break
+        }
+        catch {
+            $retryCount++
+            if ($retryCount -eq $maxRetries) {
+                Write-Verbose "Failed to run code after the maxmimum amount of $maxRetries retries."
+                throw $_
+            }
+            else {
+                Write-Verbose "Failed to run code, retrying."
+                Start-Sleep -Seconds $delaySeconds
+            }
+        }
+    }
+}
